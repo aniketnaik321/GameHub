7e8ee8a41852923e818250a2ee7db3a793436cb5
aniketnaik321 <aniket.naik321@gmail.com>
2024-06-27 01:35:20 +0530

    second commit
    second commit

--- Mobile/GameHubMobile/src/app/app.routes.ts
+++ Mobile/GameHubMobile/src/app/app.routes.ts
@@ -15,4 +15,8 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./Pages/home/home.page').then( m => m.HomePage)
   },
+  {
+    path: 'game',
+    loadComponent: () => import('./Pages/game/game.page').then( m => m.GamePage)
+  },
 ];
